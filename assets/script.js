ad41cc1baa93e9b8ea14e0f834750a21f981a43b
Akoseng <jameshii26@gmail.com>
2023-01-13 20:08:08 +0000

    update logic code for index.js

--- assets/script.js
+++ assets/script.js
@@ -90,18 +90,110 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  input("How many character of password do you want to generate?")
+  // variable to store the length of the password that user choose
+  let lenght = parseInt(
+    prompt("How many characters do you want your password to be?")
+  )
 
-}
+  if(isNaN(length) === true){
+    alert("Please input a number between 10-128");
+    return;
+  }
+
+  if(length < 10) {
+    alert("Password length must be at least 10 characters")
+  }
+
+  if(length > 65) {
+    alert("Password length must be less than 65 characters")
+  }
+
+  let incNumericCharacters = confirm (
+    "Click 'OK' to include numeric characters"
+  )
+
+  let incSpecialCharacters = confirm (
+    "Click 'OK' to include special characters"
+  )
+
+  let incLowerCaseCharacters = confirm (
+    "Click 'OK' to include lower case characters"
+  )
+
+  let incUpperCaseCharacters = confirm (
+    "Click 'OK' to include upper case characters"
+  )
+
+  if(incNumericCharacters === false &&
+    incSpecialCharacters === false &&
+    incLowerCaseCharacters === false &&
+    incUpperCaseCharacters === false) {
+      alert("At least one character option must be selected!");
+      return;
+    }
+
+    let passwordOptions = {
+      length: length,
+      incNumericCharacters: incNumericCharacters,
+      incSpecialCharacters: incSpecialCharacters,
+      incLowerCaseCharacters: incLowerCaseCharacters,
+      incUpperCaseCharacters: incUpperCaseCharacters
+    }
+
+    return passwordOptions;
+
+
+  }
+
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  let random = Math.floor(Math.random() * arr.length)
+  let ranEle = arr[random];
 
+  return ranEle;
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  getPasswordOptions();
+  let options = getPasswordOptions();
+  console.log(options);
+  let result = []
+
+  let possibleCharacters = []
+
+  let guaranteeCharacters = []
+
+  if(options.incNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteeCharacters.push(getRandom(numericCharacters))
+  }
+
+  if(options.incSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteeCharacters.push(getRandom(specialCharacters))
+  }
+
+  if(options.incLowerCaseCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteeCharacters.push(getRandom(lowerCasedCharacters))
+  }
+
+  if(options.incUpperCaseCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteeCharacters.push(getRandom(upperCasedCharacters))
+  }
+  
+  for(let i = 0; i < options.length; i++){
+    var generate = getRandom(possibleCharacters);
+    console.log(generate)
+    result.push(generate);
+  }
+
+  console.log(result);
+
+  return result.join("")
 
 }
 
